7578e1706ff4b49ee261fc763c42bb9fa88b586d
Paul Cottin <cottin.paul45@gmail.com>
2021-01-22 08:47:15 +0100

    Route to get mobile detail by id

--- src/Controller/MobileController.php
+++ src/Controller/MobileController.php
@@ -4,11 +4,35 @@
 namespace App\Controller;
 
 
+use App\Entity\Mobile;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class MobileController extends AbstractController
 {
+    /**
+     * Return mobile detail
+     * @Rest\Get(
+     *     path="/api/mobiles/{id}",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(
+     *     StatusCode = 200,
+     *     serializerGroups={"mobile"}
+     * )
+     * @param Mobile $mobile
+     * @return Mobile
+     */
+    public function mobile(Mobile $mobile)
+    {
+        if(!$mobile->getUsers()->contains($this->getUser())) {
+            throw new AccessDeniedHttpException('This mobile does not belong to you');
+        }
+
+        return $mobile;
+    }
+
     /**
      * Return the clients list linked to user connected
      * @Rest\Get(path="/api/mobiles")
